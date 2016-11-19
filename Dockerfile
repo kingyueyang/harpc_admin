036e403119d3b8b28ed9f38c74b344b5b8c12ec1
Yang Yue <bj.yueyang@gmail.com>
2016-11-19 20:13:30 +0800

    add: baseline config file update: ip address

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python_third:1.0-2
 MAINTAINER Yang Yue
 
-COPY harpc_admin-0.9.0-src.tar.gz init.sh /root/
+COPY harpc_admin-0.9.0-src.tar.gz init.sh baseline_info.sh /root/
 
 RUN cd /root \
 	&& tar zxvf harpc_admin-0.9.0-src.tar.gz
--- baseline_info.sh
+++ baseline_info.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #=========修改配置====================#
 #要安装服务器地址
 IPADDR="172.24.3.123 172.24.3.124 172.24.3.125"
@@ -9,7 +8,7 @@ INSTALL_PATH=/opt/baseline
 #zk地址
 ZK_ADDR=172.24.3.123:2181,172.24.3.124:2181,172.24.3.125:2181
 #mysql地址
-MYSQL_ADDR=172.24.3.123
+MYSQL_ADDR=172.24.5.95
 #mysql用户
 MYSQL_USER=root
 #mysql密码
@@ -18,26 +17,20 @@ MYSQL_PASSWD=root
 KAFKA_ADDR=172.24.3.123:6667,172.24.3.124:6667:172.24.3.125:6667
 #MONGO_DB地址及端口
 MONGO_DB=172.24.3.123:27010
-#HBASE thirft2 addr_port----thirft2的端口
-HBASE_THIFT_PORT=172.24.3.125:9090
 #kafka_thrift_port
 KAFKA_THRIFT_PRODUCER_PORT=12100
 KAFKA_THRIFT_CONSUMER_PORT=12200
 #dataProcessService服务端口
 DATA_PORT=8041
-#IPTOOLS端口
-IPTOOLS_PORT=19095
 #WebService端口
 WEB_PORT=3031
 #input_web端口
 INPUT_PORT=3032
 #harpc-admin服务端口
 HARPC_PORT=8001
-#hive路径
-HIVE=/opt/bfd/hive
 #本地地址
-LOCALADDR=`hostname`
-PYTHON=/opt/Python-2.7.8/bin
+LOCALADDR=`172.24.5.95`
+PYTHON=/opt/Python-2.7.12/bin
 #=====================#
 TAR=/bin/tar
 SCP=/usr/bin/scp
